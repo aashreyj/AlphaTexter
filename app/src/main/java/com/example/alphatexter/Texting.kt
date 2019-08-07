9303b834e23c77d4291012b4a5de470798535720
Aashrey <aashrey9.jain@gmail.com>
2019-08-08 00:44:03 +0530

    added default text message and custom font

--- app/src/main/java/com/example/alphatexter/Texting.kt
+++ app/src/main/java/com/example/alphatexter/Texting.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.telephony.SmsManager
+import android.text.Editable
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
